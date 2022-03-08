4f7abee9c8c97b8f6eb7fcf394882ec0633dcdcd
Jyotirmoy <jyotirmoydas12@gmail.com>
2022-03-08 08:36:48 +0530

    Added a button using styled-components which is a React-component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import './App.css';
 import Navbar from './components/header/Navbar';
+import Button from './components/Button';
+
+function clickMe(){
+    alert('Hello');
+}
 
 function App(){
     return (
@@ -12,7 +17,11 @@ function App(){
                 <div className="subheading">
                     Accelerate your business growth and boost efficiency from day one with fast and effective 24/7 IT Support and Technology Solutions.
                 </div>
-                <h4>Get in Touch</h4>
+                <div className="getInTouchBtn">
+                    <Button onClick={clickMe}>
+                        Get In Touch
+                    </Button>    
+                </div>
             </div>            
         </div>
     );       
---
+++ src/components/Button.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Button = styled.button`
+    background-color: #5c6bc0;
+    color:#eeeeee;
+    font-size: 12pt;
+    padding: 5px 15px;
+    border-radius: 18px;
+    box-shadow: 0px 10px 10px gray;
+    outline: 0;
+    cursor:pointer;
+    width:10%;
+    height:40px;
+`
+
+export default Button;
